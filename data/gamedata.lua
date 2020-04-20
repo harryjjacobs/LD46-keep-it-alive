b12ef3d086d643936ac2654a81552c4909b09860
harryjjacobs <harryjjacobs@googlemail.com>
2020-04-20 19:23:51 +0100

    Reduced spawn chance for ground powerup

--- data/gamedata.lua
+++ data/gamedata.lua
@@ -5,8 +5,8 @@ local gameData = {
     powerups = {
         {
             minScore = 0,           --score at which is starts spawning
-            maxScore = 10,          --score at which it stops spawning (optional)
-            chance = 1.0,           --probability of being spawned at any given score
+            --maxScore = 10,          --score at which it stops spawning (optional)
+            chance = 0.1,           --probability of being spawned at any given score
             lifespan = 5,           --how long until despawn if not collected (seconds)
             effectDuration = 5,     --how long the powerup is effective when applied
             entity = solidGround,   --the entity to spawn when collected (must have a .create method and :init, :deinit etc)
