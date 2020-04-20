f86d7b19faf7f5ac27d7b3985208fd1de11db03d
harryjjacobs <harryjjacobs@googlemail.com>
2020-04-20 17:43:49 +0100

    Changed large font size

--- data/fonts.lua
+++ data/fonts.lua
@@ -3,7 +3,7 @@ local fonts = {}
 function fonts:load()
     self.small = love.graphics.newFont("assets/fonts/Raleway-Regular.ttf", 32)
     self.medium = love.graphics.newFont("assets/fonts/Raleway-Regular.ttf", 64)
-    self.large = love.graphics.newFont("assets/fonts/Raleway-Regular.ttf", 100)
+    self.large = love.graphics.newFont("assets/fonts/Raleway-Regular.ttf", 120)
 end
 
 return fonts
